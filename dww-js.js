ac6c9c43f6e9b3dd2883afb2296d0c86d4760af1
GJdVos <gerwin@dewerkendewebsite.nl>
2026-09-16 13:05:06 +0200

    Update dww-js.js

--- dww-js.js
+++ dww-js.js
@@ -1,20 +1,22 @@
 /* dWW js V1.6 */
 
-/* Detect if Javascript is enabled */
+// JS & Touch detectie
 document.documentElement.classList.add("has-js")
 
-/* Detect if device is tounch */
 window.ONE = window.ONE || {};
-ONE.isTouch = (('ontouchstart' in window) || (navigator.msMaxTouchPoints > 0));
-document.documentElement.className += ONE.isTouch ? ' touch' : ' no-touch';
+ONE.isTouch = ('ontouchstart' in window) || (navigator.maxTouchPoints > 0);
+document.documentElement.classList.add(ONE.isTouch ? 'touch' : 'no-touch');
 
-/* Detect if Safari */
-/* Bron: https://www.geeksforgeeks.org/how-to-detect-the-user-browser-safari-chrome-ie-firefox-and-opera-using-javascript/ */
-let userAgentString = navigator.userAgent;
-let chromeAgent = userAgentString.indexOf("Chrome") > -1;
-let safariAgent = userAgentString.indexOf("Safari") > -1;
-if ((chromeAgent) && (safariAgent)) safariAgent = false;
-if (safariAgent) { document.documentElement.classList.add("is-safari") };
+// Safari detectie
+const userAgent = navigator.userAgent;
+const isSafari =
+	/Safari\//i.test(userAgent) &&
+	/Version\//i.test(userAgent) &&
+	!/Chrome|Chromium|CriOS|FxiOS|Edg|OPR|OPiOS|Android/i.test(userAgent);
+
+if (isSafari) {
+	document.documentElement.classList.add('is-safari');
+}
 
 /* Detect class change */
 function onClassChange(element, callback) {
@@ -29,31 +31,22 @@ function onClassChange(element, callback) {
 	return observer.disconnect;
 }
 
-document.addEventListener('DOMContentLoaded', function() {	
-    // Form submit to GA
-    document.querySelectorAll('form:not([data-form_type="search"])').forEach(form => {
-        form.addEventListener('submit', function(event) {
-            event.preventDefault();
+// Form submit to GA
+document.addEventListener('DOMContentLoaded', () => {
+	document.querySelectorAll('form:not([data-form_type="search"])').forEach(form => {
+		form.addEventListener('submit', function(event) {
+			event.preventDefault();
 
-            const succesUrl = window.location + '?dwwform=succes';
-            const succesUrlPathname = window.location.pathname + '?dwwform=succes';
+			const currentUrl = new URL(window.location.href);
+			currentUrl.searchParams.set('dwwform', 'succes');
 
-            // Google Analytics tracking
-            if ("ga" in window) {
-                const tracker = ga.getAll()[0];
-                if (tracker) {
-                    tracker.send('pageview', succesUrlPathname);
-                }
-            }
-
-            // DataLayer push
-            window.dataLayer = window.dataLayer || [];
-            dataLayer.push({
-                'event': 'virtualPageview',
-                'pageUrl': succesUrl
-            });
-        });
-    });
+			window.dataLayer = window.dataLayer || [];
+			window.dataLayer.push({
+				event: 'virtualPageview',
+				pageUrl: currentUrl.href
+			});
+		});
+	});
 });
 
 /* ---- dWW Functies ---- */
